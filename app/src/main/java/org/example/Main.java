3268c61764af6e8cafb9e47064c01b85cf1cbc02
arpit <arpitv@students.cs.ubc.ca>
2026-01-09 02:13:57 -0800

    goal achieved: run JavaFX window

---
+++ app/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("To-Do App (Setup Complete)");
+        StackPane root = new StackPane(label);
+
+        Scene scene = new Scene(root, 500, 300);
+        stage.setTitle("To-Do");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
